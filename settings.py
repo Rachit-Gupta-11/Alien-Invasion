1bfbd0fddbc9f458f222a372c9e9ff491e92893e
Rachit-Gupta-11 <gupta.11.rachit.11@gmail.com>
2026-03-10 20:41:54 +0530

    Add alien settings to settings.py

--- settings.py
+++ settings.py
@@ -13,3 +13,8 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 15
+
+        # Alien Settings
+        self.alien_speed = 1
+        self.fleet_drop_speed = 10
+        self.fleet_drop_direction = 1       # Using 1 here for right and -1 for left
