2df3843dbff5dee6c59353a324c7311d12430231
Rachit-Gupta-11 <gupta.11.rachit.11@gmail.com>
2026-03-11 11:19:46 +0530

    Adjust game settings for lives and bullet parameters

--- settings.py
+++ settings.py
@@ -6,15 +6,16 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230,230,230)
         self.ship_speed = 5
+        self.lives = 3
 
         # Bullet Settings
-        self.bullet_speed = 2
+        self.bullet_speed = 5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
-        self.bullets_allowed = 15
+        self.bullets_allowed = 50
 
         # Alien Settings
         self.alien_speed = 1
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 30
         self.fleet_drop_direction = 1       # Using 1 here for right and -1 for left
